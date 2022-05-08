576f1e7c409247d645218a393bff96596438bd41
Marcin Stryczniewicz <mstryczniewicz@gmail.com>
2022-05-09 00:14:28 +0200

    setup.sh wip

--- setup.sh
+++ setup.sh
@@ -1,5 +1,59 @@
-#! /bin/bash
-# setup zsh
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+#!/bin/bash
+
+DOTFILES_DIR=$(pwd)
+SCRIPT_NAME=$(basename "$0")
+
+if [[ ! -e $SCRIPT_NAME ]]; then
+  echo >&2 "Please cd into the cloned git repository before running $SCRIPT_NAME"
+  exit 1
+fi
+
+# make sure that the script was executed with sudo?
+#if [[ $(id -u) -eq 0 ]]
+#then
+#  echo "has sudo"
+#else
+#  echo "no sudo"
+#fi
+
+# install required software (will config files be created? need to remove before
+# creating symlinks, or install software after)
+
+# create symlinks to config files
+ln -s -t $HOME/.config $DOTFILES_DIR/.config/*
+
+# install and setup zsh
+ln -s -t $HOME $DOTFILES_DIR/.zshrc
+ln -s -t $HOME $DOTFILES_DIR/.zsh_aliases
+ln -s -t $HOME $DOTFILES_DIR/.p10k.zsh
+
+if [[ ! -d "$HOME/.oh-my-zsh" ]]
+then
+  wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+  sh install.sh --unattended --keep-zshrc
+  rm install.sh
+  # alternative aproach:
+  # sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
+fi
+
+OMZ_CUSTOM_DIR=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
+
+if [[ ! -d "$OMZ_CUSTOM_DIR/plugins/zsh-syntax-highlighting" ]]
+then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $OMZ_CUSTOM_DIR/plugins/zsh-syntax-highlighting
+fi
+
+if [[ ! -d "$OMZ_CUSTOM_DIR/plugins/zsh-autosuggestions" ]]
+then
+  git clone https://github.com/zsh-users/zsh-autosuggestions $OMZ_CUSTOM_DIR/plugins/zsh-autosuggestions
+fi
+
+if [[ ! -d "$OMZ_CUSTOM_DIR/themes/powerlevel10k" ]]
+then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $OMZ_CUSTOM_DIR/themes/powerlevel10k
+fi
+
+# check if $SHELL is set as zsh before changing it
+#chsh --shell /bin/zsh $USER
+
+# install fonts
