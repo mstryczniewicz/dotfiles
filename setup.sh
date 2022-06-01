7425de5e56d2aa9dff6bb702a429e5f0dcbfb4a0
Marcin Stryczniewicz <mstryczniewicz@gmail.com>
2022-06-01 19:49:39 +0200

    Implement best practices based on https://bash3boilerplate.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,43 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# This file:
+#
+#  - Configures a fresh arch installation
+#
+# Usage:
+#
+#  ./setup.sh
+#
+# Based on a template by BASH3 Boilerplate v2.4.1
+# http://bash3boilerplate.sh/#authors
+#
+# The MIT License (MIT)
+# Copyright (c) 2013 Kevin van Zonneveld and contributors
+# You are not obligated to bundle the LICENSE file with your b3bp projects as long
+# as you leave these references intact in the header comments of your source files.
 
 # bash best practices
 # https://kvz.io/bash-best-practices.html
-DOTFILES_DIR=$(pwd)
-SCRIPT_NAME=$(basename "$0")
 
-if [[ ! -e $SCRIPT_NAME ]]; then
-  echo >&2 "Please cd into the cloned git repository before running $SCRIPT_NAME"
-  exit 1
-fi
+# Exit on error. Append "|| true" if you expect an error.
+set -o errexit
+# Exit on error inside any functions or subshells.
+set -o errtrace
+# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
+set -o nounset
+# Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
+set -o pipefail
+# Turn on traces, useful while debugging but commented out by default
+set -o xtrace
+
+# Set magic variables for current file, directory, os, etc.
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename "${__file}" .sh)"
+
+# if [[ ! -e "${__file}" ]]; then
+#   echo >&2 "Please cd into the cloned git repository before running ${__file}"
+#   exit 1
+# fi
 
 # make sure that the script was executed with sudo?
 #if [[ $(id -u) -eq 0 ]]
@@ -41,49 +70,49 @@ sudo pacman -S --noconfirm --needed base base-devel linux linux-firmware linux-h
 # sudo systemctl enable NetworkManager
 
 # install yay
-YAY_DIR=$HOME/github/yay
-if [[ ! -d "$YAY_DIR" ]]
+__yay_dir="$HOME/github/yay"
+if [[ ! -d "${__yay_dir}" ]]
 then
-  git clone https://aur.archlinux.org/yay.git $YAY_DIR
-  cd $YAY_DIR
+  git clone "https://aur.archlinux.org/yay.git" "${__yay_dir}"
+  cd "${__yay_dir}"
   makepkg -si --noconfirm --needed
   cd -
 fi
 
 # create symlinks to config files
-mkdir -p $HOME/.config
-ln -s -t $HOME/.config $DOTFILES_DIR/.config/*
+mkdir --parents "$HOME/.config"
+ln --symbolic --target-directory="$HOME/.config" "${__dir}/.config/*" || true
 
 # install and setup zsh
-ln -s -t $HOME $DOTFILES_DIR/.zshrc
-ln -s -t $HOME $DOTFILES_DIR/.zsh_aliases
-ln -s -t $HOME $DOTFILES_DIR/.p10k.zsh
+ln --symbolic --target-directory="$HOME" "${__dir}/.zshrc" || true
+ln --symbolic --target-directory="$HOME" "${__dir}/.zsh_aliases" || true
+ln --symbolic --target-directory="$HOME" "${__dir}/.p10k.zsh" || true
 
-OMZ_DIR=${ZSH:-$HOME/.oh-my-zsh}
-if [[ ! -d "$OMZ_DIR" ]]
+__omz_main_dir="${ZSH:-$HOME/.oh-my-zsh}"
+if [[ ! -d "${__omz_main_dir}" ]]
 then
-  wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+  wget "https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
   sh install.sh --unattended --keep-zshrc
   rm install.sh
   # alternative aproach:
   # sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
 fi
 
-OMZ_CUSTOM_DIR=${ZSH_CUSTOM:-$OMZ_DIR/custom}
+__omz_custom_dir="${ZSH_CUSTOM:-${__omz_main_dir}/custom}"
 
-if [[ ! -d "$OMZ_CUSTOM_DIR/plugins/zsh-syntax-highlighting" ]]
+if [[ ! -d "${__omz_custom_dir}/plugins/zsh-syntax-highlighting" ]]
 then
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $OMZ_CUSTOM_DIR/plugins/zsh-syntax-highlighting
+  git clone "https://github.com/zsh-users/zsh-syntax-highlighting.git" "${__omz_custom_dir}/plugins/zsh-syntax-highlighting"
 fi
 
-if [[ ! -d "$OMZ_CUSTOM_DIR/plugins/zsh-autosuggestions" ]]
+if [[ ! -d "${__omz_custom_dir}/plugins/zsh-autosuggestions" ]]
 then
-  git clone https://github.com/zsh-users/zsh-autosuggestions $OMZ_CUSTOM_DIR/plugins/zsh-autosuggestions
+  git clone "https://github.com/zsh-users/zsh-autosuggestions" "${__omz_custom_dir}/plugins/zsh-autosuggestions"
 fi
 
-if [[ ! -d "$OMZ_CUSTOM_DIR/themes/powerlevel10k" ]]
+if [[ ! -d "${__omz_custom_dir}/themes/powerlevel10k" ]]
 then
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $OMZ_CUSTOM_DIR/themes/powerlevel10k
+  git clone --depth=1 "https://github.com/romkatv/powerlevel10k.git" "${__omz_custom_dir}/themes/powerlevel10k"
 fi
 
 # check if $SHELL is set as zsh before changing it
@@ -92,13 +121,14 @@ fi
 # install fonts
 
 # install ly
-LY_DIR=$HOME/github/ly
-if [[ ! -d $LY_DIR ]]
+__ly_dir="$HOME/github/ly"
+if [[ ! -d "${__ly_dir}" ]]
 then
-  git clone --recurse-submodules https://github.com/nullgemm/ly.git $LY_DIR
-  cd $LY_DIR
+  git clone --recurse-submodules "https://github.com/nullgemm/ly.git" "${__ly_dir}"
+  cd "${__ly_dir}"
   make
   sudo make install
   sudo systemctl enable ly.service
   cd -
 fi
+
