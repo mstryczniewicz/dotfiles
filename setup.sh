a1b2ce5fad022116d2d2e808422f7e44d4e35e3e
Marcin Stryczniewicz <mstryczniewicz@gmail.com>
2024-03-22 18:15:01 +0100

    Update list of software

--- setup.sh
+++ setup.sh
@@ -63,7 +63,7 @@ sudo pacman -Syyu --noconfirm
 
 # install required software (will config files be created? need to remove before
 # creating symlinks, or install software after)
-sudo pacman -S --noconfirm --needed base base-devel linux linux-firmware linux-headers grub sudo man-db xorg xf86-video-amdgpu pulseaudio pavucontrol networkmanager network-manager-applet wireless_tools wpa_supplicant os-prober mtools dosfstools dialog xdg-user-dirs wget git bspwm sxhkd picom arandr nitrogen rofi polybar zsh kitty neovim mpv keepassxc thunderbird go
+sudo pacman -S --noconfirm --needed base base-devel linux linux-firmware linux-headers grub sudo man-db xorg xf86-video-amdgpu pulseaudio pavucontrol networkmanager network-manager-applet wireless_tools wpa_supplicant os-prober mtools dosfstools dialog xdg-user-dirs wget git bspwm sxhkd picom arandr nitrogen rofi polybar zsh kitty neovim mpv keepassxc thunderbird go nnn duf
 # install picom-git with yay instead of picom from community repo?
 # install pipewire instead of pulseaudio?
 # enable networkmanager?
